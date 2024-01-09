c8286078940e4463d2f720b5d61ee5f30f8fef25
electrixky <tanyatkachuk2401@gmail.com>
2024-01-09 19:09:08 +0200

    added header

---
+++ src/components/icon/Icon.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import iconsSprite from '../../assets/images/icons-sprite.svg'
+
+type IconPropsType = {
+    iconId: string
+    width?: string
+    height?: string
+    viewBox?: string
+}
+
+const Icon = (props: IconPropsType) => {
+    return (
+        <svg width={props.width || "35"} height={props.height || "35"} viewBox={props.viewBox || "0 0 35 35"} fill="none" xmlns="http://www.w3.org/2000/svg">
+            <use xlinkHref={`${iconsSprite}#${props.iconId}`}/>
+        </svg>
+    );
+};
+
+export default Icon;
